850c00a9dc705d36dbdebfca383fa9151c8010d0
Veljko Loncarevic <veljkoloncarevicharry@gmail.com>
2024-12-03 23:59:52 +0100

    Load config

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, redirect 
 from http import HTTPStatus
+import yaml 
+
+# Need to add SQLAlchemy
 
 app = Flask(__name__)
+
+with open('config/config.yaml') as f:
+    config = yaml.load(f, yaml.Loader)
 
 @app.route("/<short_url>", methods=["GET"])
 def decodeShortURL(short_url):
     # Placeholder
-    return redirect("https://www.google.com", HTTPStatus.PERMANENT_REDIRECT)
+    return redirect(f"https://www.google.com/?q={short_url}", HTTPStatus.PERMANENT_REDIRECT)
 
 @app.route("/", methods=["POST"])
 def encodeShortURL():
